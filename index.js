30a69ee1615c3958abf799a279be8cf4713f0aa3
arriacs <acts543@gmail.com>
2025-12-11 02:11:31 -0800

    Stuffs

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-/*** You will not need this file until Unit 5 ***/
+/*** Dark Mode ***
+  
+  Purpose:
+  - Use this starter code to add a dark mode feature to your website.
+
+  When To Modify:
+  - [ ] Project 5 (REQUIRED FEATURE) 
+  - [ ] Any time after
+***/
+
+// Step 1: Select the theme button
+
+// Step 2: Write the callback function
+const toggleDarkMode = () => {
+    // Write your code here
+    // This section will run whenever the button is clicked
+}
+
+// Step 3: Register a 'click' event listener for the theme button,
+//             and tell it to use toggleDarkMode as its callback function
+
+
+/*** Form Handling [PLACEHOLDER] [ADDED IN UNIT 6] ***/
+/*** Form Handling ***
+  
+  Purpose:
+  - When the user submits the RSVP form, the name and state they 
+    entered should be added to the list of participants.
+
+  When To Modify:
+  - [ ] Project 6 (REQUIRED FEATURE)
+  - [ ] Project 6 (STRETCH FEATURE) 
+  - [ ] Project 7 (REQUIRED FEATURE)
+  - [ ] Project 9 (REQUIRED FEATURE)
+  - [ ] Any time between / after
+***/
+
+// Step 1: Add your query for the submit RSVP button here
+
+const addParticipant = (event) => {
+    // Step 2: Write your code to manipulate the DOM here
+
+    event.preventDefault();
+}
+
+// Step 3: Add a click event listener to the submit RSVP button here
+/*** Animations [PLACEHOLDER] [ADDED IN UNIT 8] ***/
+/*** Success Modal [PLACEHOLDER] [ADDED IN UNIT 9] ***/
